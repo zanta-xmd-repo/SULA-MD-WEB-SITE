5db545a6eed1547284cbbe93aca694a57d7c2aa5
sulamadara1147 <madarasulaksha@gmail.com>
2025-05-17 06:50:06 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -3,7 +3,7 @@ import * as mega from 'megajs';
 // Mega authentication credentials
 const auth = {
     email: 'madarasulaksha@gmail.com', // Replace with your Mega email
-    password: 'Sulakshamd022@ms', // Replace with your Mega password
+    password: 'Sulamd00028@msa', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
